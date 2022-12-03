43a7374da9f9114533fcff0828d30fcddc76cbf3
Guido Schmutz <guido.schmutz@trivadis.com>
2022-12-03 17:20:41 +0100

    update with logical types for date, time and timestamp

--- src/test/java/com/github/cedelsb/kafka/connect/smt/Record2JsonStringConverterTest.java
+++ src/test/java/com/github/cedelsb/kafka/connect/smt/Record2JsonStringConverterTest.java
@@ -16,14 +16,21 @@
 
 package com.github.cedelsb.kafka.connect.smt;
 
-import org.apache.kafka.connect.data.Schema;
-import org.apache.kafka.connect.data.SchemaBuilder;
-import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.common.Uuid;
+import org.apache.kafka.connect.data.*;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -59,11 +66,17 @@ public class Record2JsonStringConverterTest {
                 .field("simpleInt16", Schema.INT16_SCHEMA)
                 .field("simpleInt32", Schema.INT32_SCHEMA)
                 .field("simpleInt64", Schema.INT64_SCHEMA)
+                .field("simpleBytes", Schema.BYTES_SCHEMA)
                 .field("optionalBoolean", Schema.OPTIONAL_BOOLEAN_SCHEMA)
                 .field("optionalString", Schema.OPTIONAL_STRING_SCHEMA)
                 .field("optionalFloat", Schema.OPTIONAL_FLOAT32_SCHEMA)
                 .field("optionalInt", Schema.OPTIONAL_INT64_SCHEMA)
+                .field("optionalBytes", Schema.OPTIONAL_BYTES_SCHEMA)
                 .field("nestedArray", SchemaBuilder.array(nestedSchema))
+                .field("simpleDate", Date.SCHEMA)
+                .field("simpleTime", Time.SCHEMA)
+                .field("simpleTimestamp", Timestamp.SCHEMA)
+                .field("simpleDecimal", Decimal.schema(0))
                 .build();
 
         simpleStruct = new Struct(simpleStructSchema);
@@ -76,6 +89,7 @@ public class Record2JsonStringConverterTest {
         simpleStruct.put("simpleInt16", (short) 2);
         simpleStruct.put("simpleInt32", 3);
         simpleStruct.put("simpleInt64", 4L);
+        simpleStruct.put("simpleBytes", new byte[]{75, 97, 102, 107, 97, 32, 114, 111, 99, 107, 115, 33});
 
         final Struct simpleNestedStruct1 = new Struct(nestedSchema);
         simpleNestedStruct1.put("entry", "testEntry");
@@ -85,6 +99,11 @@ public class Record2JsonStringConverterTest {
         final List<Struct> nestedStructArray = Arrays.asList(simpleNestedStruct1, simpleNestedStruct2);
 
         simpleStruct.put("nestedArray", nestedStructArray);
+
+        simpleStruct.put("simpleDate",java.util.Date.from(ZonedDateTime.of(LocalDate.of(2022,12,3), LocalTime.MIDNIGHT, ZoneOffset.UTC).toInstant()));
+        simpleStruct.put("simpleTime",java.util.Date.from(ZonedDateTime.of(LocalDate.of(2022,12,3), LocalTime.MIDNIGHT, ZoneOffset.UTC).toInstant()));
+        simpleStruct.put("simpleTimestamp",java.util.Date.from(ZonedDateTime.of(LocalDate.of(2022,12,3), LocalTime.NOON, ZoneOffset.UTC).toInstant()));
+        simpleStruct.put("simpleDecimal", new BigDecimal("12345.6789"));
     }
 
     @Test
@@ -104,7 +123,7 @@ public class Record2JsonStringConverterTest {
         Struct value = (Struct) transformedRecord.value();
         String jsonString = (String) value.get("myawesomejsonstringfield");
 
-        assertEquals("{\"simpleString\": \"TestString\", \"simpleBoolean\": true, \"simpleFLOAT32\": 1.0, \"simpleFLOAT64\": 2.0, \"simpleInt8\": 8, \"simpleInt16\": 2, \"simpleInt32\": 3, \"simpleInt64\": 4, \"optionalBoolean\": null, \"optionalString\": null, \"optionalFloat\": null, \"optionalInt\": null, \"nestedArray\": [{\"entry\": \"testEntry\"}, {\"entry\": \"testEntry2\"}]}",jsonString);
+        assertEquals("{\"simpleString\": \"TestString\", \"simpleBoolean\": true, \"simpleFLOAT32\": 1.0, \"simpleFLOAT64\": 2.0, \"simpleInt8\": 8, \"simpleInt16\": 2, \"simpleInt32\": 3, \"simpleInt64\": 4, \"simpleBytes\": {\"$binary\": {\"base64\": \"S2Fma2Egcm9ja3Mh\", \"subType\": \"00\"}}, \"optionalBoolean\": null, \"optionalString\": null, \"optionalFloat\": null, \"optionalInt\": null, \"optionalBytes\": null, \"nestedArray\": [{\"entry\": \"testEntry\"}, {\"entry\": \"testEntry2\"}], \"simpleDate\": {\"$date\": \"2022-12-03T00:00:00Z\"}, \"simpleTime\": {\"$date\": \"2022-12-03T00:00:00Z\"}, \"simpleTimestamp\": {\"$date\": \"2022-12-03T12:00:00Z\"}, \"simpleDecimal\": {\"$numberDecimal\": \"12345.6789\"}}",jsonString);
     }
 
     @Test
@@ -125,7 +144,28 @@ public class Record2JsonStringConverterTest {
         Struct value = (Struct) transformedRecord.value();
         String jsonString = (String) value.get("myawesomejsonstringfield");
 
-        assertEquals("{\"simpleString\": \"TestString\", \"simpleBoolean\": true, \"simpleFLOAT32\": {\"$numberDouble\": \"1.0\"}, \"simpleFLOAT64\": {\"$numberDouble\": \"2.0\"}, \"simpleInt8\": {\"$numberInt\": \"8\"}, \"simpleInt16\": {\"$numberInt\": \"2\"}, \"simpleInt32\": {\"$numberInt\": \"3\"}, \"simpleInt64\": {\"$numberLong\": \"4\"}, \"optionalBoolean\": null, \"optionalString\": null, \"optionalFloat\": null, \"optionalInt\": null, \"nestedArray\": [{\"entry\": \"testEntry\"}, {\"entry\": \"testEntry2\"}]}",jsonString);
+        assertEquals("{\"simpleString\": \"TestString\", \"simpleBoolean\": true, \"simpleFLOAT32\": {\"$numberDouble\": \"1.0\"}, \"simpleFLOAT64\": {\"$numberDouble\": \"2.0\"}, \"simpleInt8\": {\"$numberInt\": \"8\"}, \"simpleInt16\": {\"$numberInt\": \"2\"}, \"simpleInt32\": {\"$numberInt\": \"3\"}, \"simpleInt64\": {\"$numberLong\": \"4\"}, \"simpleBytes\": {\"$binary\": {\"base64\": \"S2Fma2Egcm9ja3Mh\", \"subType\": \"00\"}}, \"optionalBoolean\": null, \"optionalString\": null, \"optionalFloat\": null, \"optionalInt\": null, \"optionalBytes\": null, \"nestedArray\": [{\"entry\": \"testEntry\"}, {\"entry\": \"testEntry2\"}], \"simpleDate\": {\"$date\": {\"$numberLong\": \"1670025600000\"}}, \"simpleTime\": {\"$date\": {\"$numberLong\": \"1670025600000\"}}, \"simpleTimestamp\": {\"$date\": {\"$numberLong\": \"1670068800000\"}}, \"simpleDecimal\": {\"$numberDecimal\": \"12345.6789\"}}",jsonString);
+    }
+
+    @Test
+    public void transformRecordValue2JsonStringSTRICTModeTest() {
+        final Map<String, Object> props = new HashMap<>();
+
+        props.put("json.string.field.name", "myawesomejsonstringfield");
+        props.put("json.writer.output.mode", "STRICT");
+
+        valueSmt.configure(props);
+
+        final SinkRecord record = new SinkRecord(null, 0, null, "test", simpleStructSchema, simpleStruct, 0);
+        final SinkRecord transformedRecord = valueSmt.apply(record);
+
+        assertEquals(1,transformedRecord.valueSchema().fields().size());
+        assertEquals(Schema.STRING_SCHEMA, transformedRecord.valueSchema().field("myawesomejsonstringfield").schema());
+
+        Struct value = (Struct) transformedRecord.value();
+        String jsonString = (String) value.get("myawesomejsonstringfield");
+
+        assertEquals("{\"simpleString\": \"TestString\", \"simpleBoolean\": true, \"simpleFLOAT32\": 1.0, \"simpleFLOAT64\": 2.0, \"simpleInt8\": 8, \"simpleInt16\": 2, \"simpleInt32\": 3, \"simpleInt64\": {\"$numberLong\": \"4\"}, \"simpleBytes\": {\"$binary\": \"S2Fma2Egcm9ja3Mh\", \"$type\": \"00\"}, \"optionalBoolean\": null, \"optionalString\": null, \"optionalFloat\": null, \"optionalInt\": null, \"optionalBytes\": null, \"nestedArray\": [{\"entry\": \"testEntry\"}, {\"entry\": \"testEntry2\"}], \"simpleDate\": {\"$date\": 1670025600000}, \"simpleTime\": {\"$date\": 1670025600000}, \"simpleTimestamp\": {\"$date\": 1670068800000}, \"simpleDecimal\": {\"$numberDecimal\": \"12345.6789\"}}",jsonString);
     }
 
     @Test
@@ -146,7 +186,7 @@ public class Record2JsonStringConverterTest {
         Struct value = (Struct) transformedRecord.value();
         String jsonString = (String) value.get("myawesomejsonstringfield");
 
-        assertEquals("{\"simpleString\": \"TestString\", \"simpleBoolean\": true, \"simpleFLOAT32\": {\"$numberDouble\": \"1.0\"}, \"simpleFLOAT64\": {\"$numberDouble\": \"2.0\"}, \"simpleInt8\": {\"$numberInt\": \"8\"}, \"simpleInt16\": {\"$numberInt\": \"2\"}, \"simpleInt32\": {\"$numberInt\": \"3\"}, \"simpleInt64\": {\"$numberLong\": \"4\"}, \"optionalBoolean\": null, \"optionalString\": null, \"optionalFloat\": null, \"optionalInt\": null, \"nestedArray\": [{\"entry\": \"testEntry\"}, {\"entry\": \"testEntry2\"}]}",jsonString);
+        assertEquals("{\"simpleString\": \"TestString\", \"simpleBoolean\": true, \"simpleFLOAT32\": {\"$numberDouble\": \"1.0\"}, \"simpleFLOAT64\": {\"$numberDouble\": \"2.0\"}, \"simpleInt8\": {\"$numberInt\": \"8\"}, \"simpleInt16\": {\"$numberInt\": \"2\"}, \"simpleInt32\": {\"$numberInt\": \"3\"}, \"simpleInt64\": {\"$numberLong\": \"4\"}, \"simpleBytes\": {\"$binary\": {\"base64\": \"S2Fma2Egcm9ja3Mh\", \"subType\": \"00\"}}, \"optionalBoolean\": null, \"optionalString\": null, \"optionalFloat\": null, \"optionalInt\": null, \"optionalBytes\": null, \"nestedArray\": [{\"entry\": \"testEntry\"}, {\"entry\": \"testEntry2\"}], \"simpleDate\": {\"$date\": {\"$numberLong\": \"1670025600000\"}}, \"simpleTime\": {\"$date\": {\"$numberLong\": \"1670025600000\"}}, \"simpleTimestamp\": {\"$date\": {\"$numberLong\": \"1670068800000\"}}, \"simpleDecimal\": {\"$numberDecimal\": \"12345.6789\"}}",jsonString);
     }
 
     @Test
@@ -253,16 +293,35 @@ public class Record2JsonStringConverterTest {
                 "  <simpleInt16 number=\"true\">2</simpleInt16>\n" +
                 "  <simpleInt32 number=\"true\">3</simpleInt32>\n" +
                 "  <simpleInt64 number=\"true\">4</simpleInt64>\n" +
+                "  <simpleBytes>\n" +
+                "    <__EQ__binary>\n" +
+                "      <base64>S2Fma2Egcm9ja3Mh</base64>\n" +
+                "      <subType>00</subType>\n" +
+                "    </__EQ__binary>\n" +
+                "  </simpleBytes>\n" +
                 "  <optionalBoolean null=\"true\"/>\n" +
                 "  <optionalString null=\"true\"/>\n" +
                 "  <optionalFloat null=\"true\"/>\n" +
                 "  <optionalInt null=\"true\"/>\n" +
+                "  <optionalBytes null=\"true\"/>\n" +
                 "  <nestedArray>\n" +
                 "    <entry>testEntry</entry>\n" +
                 "  </nestedArray>\n" +
                 "  <nestedArray>\n" +
                 "    <entry>testEntry2</entry>\n" +
                 "  </nestedArray>\n" +
+                "  <simpleDate>\n" +
+                "    <__EQ__date>2022-12-03T00:00:00Z</__EQ__date>\n" +
+                "  </simpleDate>\n" +
+                "  <simpleTime>\n" +
+                "    <__EQ__date>2022-12-03T00:00:00Z</__EQ__date>\n" +
+                "  </simpleTime>\n" +
+                "  <simpleTimestamp>\n" +
+                "    <__EQ__date>2022-12-03T12:00:00Z</__EQ__date>\n" +
+                "  </simpleTimestamp>\n" +
+                "  <simpleDecimal>\n" +
+                "    <__EQ__numberDecimal>12345.6789</__EQ__numberDecimal>\n" +
+                "  </simpleDecimal>\n" +
                 "</root>",jsonString);
     }
 }
